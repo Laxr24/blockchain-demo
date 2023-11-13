4e7101e6c821e2337d0feb4b7cf915f87a05261a
Laxr24 <73450829+Laxr24@users.noreply.github.com>
2023-11-13 11:03:39 +0600

    Updated
    update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 var async = require('async');
 
 router.get('/', function(req, res, next) {
-  res.render('index');
+  // res.render('index');
+  res.send("Welcome to BlockChain Demo")
 });
 
 router.get('/:page', function(req, res, next) {
